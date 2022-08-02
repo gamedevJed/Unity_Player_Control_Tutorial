f232005eef3c734ecaf842e4111b0bad36b615e8
gamedevJed <109969084+gamedevJed@users.noreply.github.com>
2022-08-02 13:26:27 -0500

    Upload project assets

---
+++ Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 cameraOffset = new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // use late update to get rid of stutter
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + cameraOffset;
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // speed variable
+    public float speed = 20;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+       
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // move vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
